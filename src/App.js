e9df8ce8c244ba7a7c14fbe4d8859c5ea6a064b2
zelalem Habtegiorgis <zeegiorgis@gmail.com>
2023-01-04 15:47:05 -0500

    fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import Axios from 'axios'
+import { useEffect, useState } from 'react'
+// we have to use .then() because the fetch function returns a promise. it needs to be converted to js object bcause we get it as a JSON.
+// Axios.get will be showing everytime the component updates
 function App() {
+  // fetch('https://catfact.ninja/fact')
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     console.log(data)
+  //   })
+  const [catFact, setCatFact] = useState('')
+  // function that will be fetchig fetchCatFact that is to link with the button onClick to get the fetchdata.
+  const fetchCatFact = () => {
+    Axios.get('https://catfact.ninja/fact').then((res) => {
+      setCatFact(res.data.fact)
+      console.log(res.data)
+    })
+  }
+  useEffect(() => {
+    // Axios.get('https://catfact.ninja/fact').then((res) => {
+    //   setCatFact(res.data.fact)
+    //   console.log(res.data)
+    fetchCatFact() // function call; this will work the same way. since we put the same inside the function possible to substitue with calling function.
+    // })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchCatFact}> Generate Cat Fact </button>
+      <p>{catFact} </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// we remove the StrictMode because we don't want to fetch the data twice
